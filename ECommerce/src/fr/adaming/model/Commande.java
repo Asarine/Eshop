31e14034d0536327d963d1771bafab48ad17a3a8
inti0426 <inti0426@DESKTOP-5QL27UL>
2018-03-29 14:48:44 +0200

    Phanuelle : commit administrateur, produit, commande.

--- ECommerce/src/fr/adaming/model/Commande.java
+++ ECommerce/src/fr/adaming/model/Commande.java
@@ -1,6 +1,59 @@
 package fr.adaming.model;
 
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="commandes")
 public class Commande {
+	
+	// Attributs
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id_com")
+	private Long idCommande;
+	private Date date;
+	
+	// Constructeur
+	public Commande() {
+		super();
+	}
+	public Commande(Date date) {
+		super();
+		this.date = date;
+	}
+	public Commande(Long idCommande, Date date) {
+		super();
+		this.idCommande = idCommande;
+		this.date = date;
+	}
+	
+	// Getters et setters
+	public Long getIdCommande() {
+		return idCommande;
+	}
+	public void setIdCommande(Long idCommande) {
+		this.idCommande = idCommande;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+	// Génération toString
+	@Override
+	public String toString() {
+		return "Commande [idCommande=" + idCommande + ", date=" + date + "]";
+	}
+	
 	
 
 }
---
+++ ECommerce/src/fr/adaming/model/Produit.java
@@ -0,0 +1,105 @@
+package fr.adaming.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="produits")
+public class Produit {
+
+	// Attributs
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id_pr")
+	private Long idProduit;
+	private String designation;
+	private String description;
+	private double prix;
+	private int quantite;
+	private boolean selectionne;
+	private byte photo;
+	
+	
+	// Constructeurs
+	public Produit() {
+		super();
+	}
+	public Produit(String designation, String description, double prix, int quantité, boolean selectionne, byte photo) {
+		super();
+		this.designation = designation;
+		this.description = description;
+		this.prix = prix;
+		this.quantite = quantite;
+		this.selectionne = selectionne;
+		this.photo = photo;
+	}
+	public Produit(Long idProduit, String designation, String description, double prix, int quantité,
+			boolean selectionne, byte photo) {
+		super();
+		this.idProduit = idProduit;
+		this.designation = designation;
+		this.description = description;
+		this.prix = prix;
+		this.quantite = quantite;
+		this.selectionne = selectionne;
+		this.photo = photo;
+	}
+	
+	// Getters et setters
+	public Long getIdProduit() {
+		return idProduit;
+	}
+	public void setIdProduit(Long idProduit) {
+		this.idProduit = idProduit;
+	}
+	public String getDesignation() {
+		return designation;
+	}
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public double getPrix() {
+		return prix;
+	}
+	public void setPrix(double prix) {
+		this.prix = prix;
+	}
+	public int getQuantite() {
+		return quantite;
+	}
+	public void setQuantité(int quantite) {
+		this.quantite = quantite;
+	}
+	public boolean isSelectionne() {
+		return selectionne;
+	}
+	public void setSelectionne(boolean selectionne) {
+		this.selectionne = selectionne;
+	}
+	public byte getPhoto() {
+		return photo;
+	}
+	public void setPhoto(byte photo) {
+		this.photo = photo;
+	}
+	
+	// Rédéfinition toString
+	@Override
+	public String toString() {
+		return "Produit [idProduit=" + idProduit + ", designation=" + designation + ", description=" + description
+				+ ", prix=" + prix + ", quantite=" + quantite + ", selectionne=" + selectionne + ", photo=" + photo
+				+ "]";
+	}
+	
+	
+}
